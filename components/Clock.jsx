04028bcb7d9918248d9d859daa40ad7170529cc6
alex-reyne <creativereyne@gmail.com>
2022-05-16 02:51:51 -0600

    add clock component

---
+++ components/Clock.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+import styles from '../styles/Clock.module.scss';
+
+export default function Clock() {
+  const [dateTime, setDateTime] = useState('');
+  const updateClock = () => {
+    const days = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
+
+    let date = new Date();
+
+    const dateString = `
+    ${days[date.getDay()]} ${date.getDay()}/${date.getMonth()}/${date.getFullYear()}
+    ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}
+  `;
+
+    setTimeout(() => {
+      updateClock();
+    }, 1000);
+
+    setDateTime(dateString);
+  };
+
+  useEffect(() => {
+    updateClock();
+  }, []);
+
+  return <p className={styles.time}>{dateTime}</p>;
+}
