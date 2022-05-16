bd8916cf7162d8b1b68d58864ad577671ad64b05
alex-reyne <creativereyne@gmail.com>
2022-05-16 14:10:08 -0600

    create new link form component

---
+++ components/AddForm.jsx
@@ -0,0 +1,50 @@
+import styles from '../styles/AddForm.module.scss';
+
+export default function AddForm({ setAdd, addForm, setAddForm }) {
+  return (
+    <form className={styles.linkform}>
+      <div>
+        <input
+          type="text"
+          value={addForm.name}
+          placeholder="name"
+          onChange={(e) => {
+            setAddForm((prev) =>
+              prev ? { ...prev, name: e.target.value } : { name: e.target.value }
+            );
+          }}
+        />
+        <input
+          type="text"
+          value={addForm.url}
+          placeholder="url"
+          onChange={(e) => {
+            setAddForm((prev) =>
+              prev ? { ...prev, url: e.target.value } : { url: e.target.value }
+            );
+          }}
+        />
+      </div>
+
+      <div>
+        <button
+          onClick={(e) => {
+            newLink(setLinks, addForm);
+            setAddForm({});
+            setAdd(false);
+          }}
+        >
+          Add
+        </button>
+        <button
+          onClick={(e) => {
+            setAddForm({});
+            setAdd(false);
+          }}
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
+  );
+}
