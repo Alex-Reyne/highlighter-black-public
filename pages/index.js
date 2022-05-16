973c76f12ded6f495717c86fed51ec9a6ad61dbc
alex-reyne <creativereyne@gmail.com>
2022-05-16 02:07:24 -0600

    add search bar and image upload

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,6 @@ export default function Home() {
     { name: 'link 1', url: 'https://google.ca' },
     { name: 'link 1', url: 'https://google.ca' },
     { name: 'link 1', url: 'https://google.ca' },
-    { name: 'link 1', url: 'https://google.ca' },
-    { name: 'link 1', url: 'https://google.ca' },
   ];
 
   const linkList = links.map((link) => {
@@ -48,12 +46,35 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <section className={styles.content__box}>
-          <h1 className={styles.title}>ケツ &gt;</h1>
-          <section className={styles.links}>
-            <ul className={styles.link__list}>{linkList}</ul>
+        <div>
+          <section className={styles.content__box}>
+            <h1 className={styles.title}>ケツ &gt;</h1>
+            <section className={styles.links}>
+              <ul className={styles.link__list}>{linkList}</ul>
+            </section>
           </section>
-        </section>
+          <section>
+            <label htmlFor="file-input">
+              <img id="img" src={'images/stars.gif'} alt="user_image" />
+            </label>
+
+            <input
+              id={styles.file__input}
+              type="file"
+              onChange={(e) => submitImage(e, setImage, REACT_APP_IMGBB)}
+            />
+          </section>
+        </div>
+        <form
+          id={styles.search}
+          name="form1"
+          action="https://duckduckgo.com/?q="
+          target="_blank"
+          method="get"
+          onSubmit={(e) => reset(e)}
+        >
+          <input type="text" placeholder="Duck Duck Go Search..." name="q" />
+        </form>
       </main>
 
       <footer className={styles.footer}>
