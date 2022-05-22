6b9a1c28c4b90b18c18dead97d7a039e9837f1a6
alex-reyne <creativereyne@gmail.com>
2022-05-22 10:02:24 -0600

    add api key to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_IMGBB_KEY: process.env.NEXT_PUBLIC_IMGBB_KEY,
+  },
 };
 
 module.exports = nextConfig;
